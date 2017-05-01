d11400ab8113946336054c0b9e0c1af813dc35e8
Jan Sourek <jan.sourek@gmail.com>
2017-05-01 22:19:44 +0200

    Names added

---
+++ names.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+# coding: utf-8
+
+print "Jména lektorů kurzu:"
+
+lecturers = [
+	"Honza",
+	"Vlasta",
+	"Bizi",
+	"Míra"
+	]
+
+for name in lecturers:
+	print name
+
+print "\nJména účastníků kurzu:"
